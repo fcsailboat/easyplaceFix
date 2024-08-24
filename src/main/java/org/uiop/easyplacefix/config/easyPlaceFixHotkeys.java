9692c98a51a0f5a3c2a74fe7d3b0c93f11008ae1
zna15 <96153949223225zzzkkk@users.noreply.github.com>
2024-08-25 00:09:46 +0800

    添加了忽视背包内物品NBT组件的配置项

--- src/main/java/org/uiop/easyplacefix/config/easyPlaceFixHotkeys.java
+++ src/main/java/org/uiop/easyplacefix/config/easyPlaceFixHotkeys.java
@@ -7,15 +7,20 @@ import net.minecraft.client.MinecraftClient;
 public final class easyPlaceFixHotkeys {
     public static final ConfigHotkey LOOSEN_MODE_HOTKEY =new ConfigHotkey("loosenModeHotkey","","easyPlaceFix.config.hotkeys.comment.loosenmode")
             .translatedName("easyPlaceFix.config.hotkeys.name.loosenmode");
+    public static final ConfigHotkey IGNORE_NBT_HOTKEY =new ConfigHotkey("nbtIgnoreHotkey","","easyPlaceFix.config.hotkeys.comment.nbtIgnore")
+            .translatedName("easyPlaceFix.config.hotkeys.name.nbtIgnore");
 
     public static ConfigHotkey[] getExtraHotkeys() {
         return new ConfigHotkey[] {
                 LOOSEN_MODE_HOTKEY,
+                IGNORE_NBT_HOTKEY,
         };
     }
 
     public static void addCallbacks(MinecraftClient mc) {
         LOOSEN_MODE_HOTKEY.getKeybind().setCallback(new KeyCallbackToggleBooleanConfigWithMessage(easyPlacefixConfig.LOOSEN_MODE));
+        IGNORE_NBT_HOTKEY.getKeybind().setCallback(new KeyCallbackToggleBooleanConfigWithMessage(easyPlacefixConfig.IGNORE_NBT));
+
     }
 
 }
--- src/main/java/org/uiop/easyplacefix/config/easyPlacefixConfig.java
+++ src/main/java/org/uiop/easyplacefix/config/easyPlacefixConfig.java
@@ -5,9 +5,12 @@ import fi.dy.masa.malilib.config.options.ConfigBoolean;
 public final class easyPlacefixConfig {
     public static final ConfigBoolean LOOSEN_MODE   = new ConfigBoolean("loosenMode", false, "EasyPlaceFix.config.generic.comment.loosenMode")
             .translatedName("EasyPlaceFix.config.generic.name.loosenMode");
+    public static final ConfigBoolean IGNORE_NBT = new ConfigBoolean("nbtIgnore", false, "EasyPlaceFix.config.generic.comment.nbtIgnore")
+            .translatedName("EasyPlaceFix.config.generic.name.nbtIgnore");
     public static IConfigBase[] getExtraGenericConfigs() {
         return new IConfigBase[] {
                 LOOSEN_MODE,
+                IGNORE_NBT,
         };
     }
 }
