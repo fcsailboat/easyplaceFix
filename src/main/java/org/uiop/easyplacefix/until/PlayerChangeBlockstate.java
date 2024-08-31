2582848a2452e291034ea351ddd8257bc17201db
zna15 <96153949223225zzzkkk@users.noreply.github.com>
2024-09-01 02:08:17 +0800

    封装了交互方块的方法

---
+++ src/main/java/org/uiop/easyplacefix/until/PlayerChangeBlockstate.java
@@ -0,0 +1,14 @@
+package org.uiop.easyplacefix.until;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+
+public class PlayerChangeBlockstate {
+    public static void InteractBlock(int InteractCount, BlockHitResult trace){
+        for (int i=0;i<InteractCount-1;i++){
+            MinecraftClient.getInstance().getNetworkHandler().sendPacket(new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND,trace,i));
+        }
+    }
+}
