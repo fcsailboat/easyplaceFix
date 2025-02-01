ccbed5e2c31cadea20d46ed2adcb805c7edefc37
zna15 <96153949223225zzzkkk@users.noreply.github.com>
2025-02-01 19:42:36 +0800

    添加注释

--- src/main/java/org/uiop/easyplacefix/until/doEasyPlace.java
+++ src/main/java/org/uiop/easyplacefix/until/doEasyPlace.java
@@ -11,10 +11,10 @@ public class doEasyPlace {//TODO 轻松放置重写计划
     // 是否在原理图范围内
     public static boolean isSchematicBlock(BlockPos pos) {
         SchematicPlacementManager schematicPlacementManager = DataManager.getSchematicPlacementManager();
-
+        //获取该pos所在区块已加载原理图集合
         List<SchematicPlacementManager.PlacementPart> allPlacementsTouchingChunk
                 = schematicPlacementManager.getAllPlacementsTouchingChunk(pos);
-
+        //便利集合中的原理图是否覆盖了该pos
         for (SchematicPlacementManager.PlacementPart placementPart : allPlacementsTouchingChunk) {
             if (placementPart.getBox().containsPos(pos)) {
                 return true;
