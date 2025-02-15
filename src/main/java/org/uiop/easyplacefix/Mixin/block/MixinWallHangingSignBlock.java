fdce3d92b5707d1ba980843abed07e14e94b550b
zna15 <96153949223225zzzkkk@users.noreply.github.com>
2025-02-15 23:15:47 +0800

    修复壁挂式的告示牌朝向问题

--- src/main/java/org/uiop/easyplacefix/Mixin/block/MixinWallHangingSignBlock.java
+++ src/main/java/org/uiop/easyplacefix/Mixin/block/MixinWallHangingSignBlock.java
@@ -12,13 +12,29 @@ import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.uiop.easyplacefix.IBlock;
+import org.uiop.easyplacefix.LookAt;
 
 @Mixin(WallHangingSignBlock.class)
 public abstract class MixinWallHangingSignBlock implements IBlock {
     @Shadow
     public abstract boolean canAttachAt(BlockState state, WorldView world, BlockPos pos);
-//TODO 这里可以不用朝向数据包，但是我没看懂她是怎么判断周围有没有可以依附的方块的，暂时先发送朝向数据包
 
+    @Override
+    public long sleepTime(BlockState blockState) {
+        return 50_000_000;
+    }
+
+    //TODO 这里可以不用朝向数据包，但是我没看懂她是怎么判断周围有没有可以依附的方块的，暂时先发送朝向数据包
+//这里要发送朝向数据包，因为朝向不同文字的面也不同
+@Override
+public Pair<LookAt, LookAt> getYawAndPitch(BlockState blockState) {
+    return switch (blockState.get(Properties.HORIZONTAL_FACING)) {
+        case SOUTH -> new Pair<>(LookAt.North, LookAt.GetNow);
+        case WEST -> new Pair<>(LookAt.East, LookAt.GetNow);
+        case EAST -> new Pair<>(LookAt.West, LookAt.GetNow);
+        default -> new Pair<>(LookAt.South, LookAt.GetNow);
+    };
+}
 //    @Override
 //    public Pair<LookAt, LookAt> getYawAndPitch(BlockState blockState) {
 //        switch (blockState.get(Properties.FACING)){
