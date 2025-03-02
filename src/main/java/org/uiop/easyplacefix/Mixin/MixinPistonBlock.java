36ba0889503a0c0d98b663c226c10dd240895891
zna15 <96153949223225zzzkkk@users.noreply.github.com>
2025-03-02 20:46:57 +0800

    测试

--- src/main/java/org/uiop/easyplacefix/Mixin/MixinPistonBlock.java
+++ src/main/java/org/uiop/easyplacefix/Mixin/MixinPistonBlock.java
@@ -5,16 +5,18 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.PistonBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.uiop.easyplacefix.EasyPlaceFix;
+
+import static org.uiop.easyplacefix.until.PlayerBlockAction.useItemOnAction.modifyBoolean;
+import static org.uiop.easyplacefix.until.PlayerBlockAction.useItemOnAction.pistonBlockState;
 
 @Mixin(PistonBlock.class)
 public class MixinPistonBlock {//这个mixin是对客户端获取活塞状态的同步（直接发送数据包将使服务端和客户端不同步..暂时）
 
     @ModifyReturnValue(method = "getPlacementState", at = @At(value = "RETURN"))
     private BlockState ModgetPlacementState(BlockState original) {
-        if (EasyPlaceFix.modifyBoolean) {//仅在放置活塞时需要修改
-            EasyPlaceFix.modifyBoolean = false;
-            return EasyPlaceFix.pistonBlockState;//投影的方块状态
+        if (modifyBoolean) {//仅在放置活塞时需要修改
+            modifyBoolean = false;
+            return pistonBlockState;//投影的方块状态
         }
         return original;
     }
